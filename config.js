c62cf6c5a7bdf21c81a9a6129882cbd3355a6141
Ady <ady@gms-macbook-pro.lan>
2020-12-08 17:35:30 -0800

    client origin update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://rocky-peak-50014.herokuapp.com',
+    CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://petful-client-seven.vercel.app',
   } 
